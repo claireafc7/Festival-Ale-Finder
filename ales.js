6620774cf93b376d4384c25dabf68c20271f1553
claireafc7 <clairequinn97123@gmail.com>
2024-10-07 00:43:02 +0100

    Add files via upload

---
+++ ales.js
@@ -0,0 +1,159 @@
+// ales.js
+export const ales = [
+    {
+        name: "Red Racer Session IPA",
+        strengthCategory: 4.0,
+        strength: "Between 3.5% ABV and 4.0% ABV",
+        colour: "Amber, Mid Brown",
+        flavour: ["Hoppy", "Balanced", "Malty", "Fruity", "Refreshing"]
+    },
+    {
+        name: "Dinner Ale",
+        strengthCategory: 4.2,
+        strength: "Between 4.1% ABV and 4.5% ABV",
+        colour: "Amber, Mid Brown",
+        flavour: ["Hoppy", "Malty", "Smooth", "Nutty"]
+    },
+    {
+        name: "Orihime Pale Ale",
+        strengthCategory: 4.3,
+        strength: "Between 4.1% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Hoppy", "Malty", "Citrusy", "Fruity", "Refreshing"]
+    },
+    {
+        name: "Who Dat",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.0% ABV",
+        colour: "Golden",
+        flavour: ["Crisp", "Hoppy", "Refreshing", "Fruity"]
+    },
+    {
+        name: "All Dog Alert",
+        strengthCategory: 5.5,
+        strength: "Between 5.1% ABV and 5.5% ABV",
+        colour: "Black",
+        flavour: ["Rich", "Smooth", "Malty", "Chocolatey"]
+    },
+    
+        {
+        name: "Canberra",
+        strengthCategory: 4,
+        strength: "Between 3.5% ABV and 4.0% ABV",
+        colour: "Ruby, Dark Brown",
+        flavour: ["Chocolatey", "Fruity", "Sweet"]
+    },
+    
+    {
+        name: "Salem Session",
+        strengthCategory: 4.1,
+        strength: "Between 4.1% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Hoppy", "Balanced", "Bitter"]
+    },
+     {
+        name: "Mango in the Night",
+        strengthCategory: 4.5,
+        strength: "Between 4.1% ABV and 4.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Fruity", "Smooth", "Sweet"]
+    },
+    
+    {
+        name: "Phoneix",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.0% ABV",
+        colour: "Amber Mid Brown",
+        flavour: ["Caramel", "Spicy", "Fruity"]
+    },
+    
+  {
+        name: "Carribean Rum Cask",
+        strengthCategory: 6.8,
+        strength: "Above 5.5% ABV",
+        colour: "Ruby, Dark Brown",
+        flavour: ["Spicy", "Malty", "Fruity", "Sweet"]
+    },
+        
+    {
+        name: "Born to be Mild",
+        strengthCategory: 3.8,
+        strength: "Between 3.5% ABV and 4.0% ABV",
+        colour: "Ruby, Dark Brown",
+        flavour: ["Chocolatey", "Coffee", "Sweet", "Malty"]
+    },
+    
+        {
+        name: "Sun Lounger",
+        strengthCategory: 4.4,
+        strength: "Between 4.0% ABV and 4.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Fruity", "Citrusy", "Refreshing"]
+    },
+    
+        {
+        name: "Sapphire Spoon",
+        strengthCategory: 4.7,
+        strength: "Between 4.6% ABV and 5.0% ABV",
+        colour: "Amber, Mid Brown",
+        flavour: ["Malty", "Bitter", "Sweet"]
+    },
+    
+        {
+        name: "Fresh Pot",
+        strengthCategory: 4.8,
+        strength: "Between 4.6% ABV and 5.0% ABV",
+        colour: "Black",
+        flavour: ["Coffee", "Chocolatey", "Caramel", "Rich"]
+    },
+    
+        {
+        name: "Aurora",
+        strengthCategory: 5.6,
+        strength: "Above 5.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Malty", "Citrusy", "Fruity"]
+    },
+    
+        {
+        name: "T Drop",
+        strengthCategory: 4.0,
+        strength: "Between 3.5% ABV and 4.0% ABV",
+        colour: "Golden",
+        flavour: ["Citrusy", "Sweet", "Fruity", "Floral"]
+    },
+    
+        {
+        name: "Tower Ale",
+        strengthCategory: 4.2,
+        strength: "Between 4.1% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Fruity", "Caramel", "Sweet", "Floral"]
+    },
+    
+        {
+        name: "The Gloaming",
+        strengthCategory: 4.4,
+        strength: "Between 4.1% ABV and 4.5% ABV",
+        colour: "Ruby, Dark Brown",
+        flavour: ["Chocolatey", "Caramel", "Malty"]
+    },
+    
+        {
+        name: "Hop Storm",
+        strengthCategory: 4.8,
+        strength: "Between 4.6% ABV and 5.0% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Hoppy", "Fruity", "Citrusy"]
+    },
+    
+        {
+        name: "Coltrane",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.0% ABV",
+        colour: "Amber, Mid Brown",
+        flavour: ["Caramel", "Sweet", "Hoppy", "Citrusy"]
+    },
+    
+       
+];
